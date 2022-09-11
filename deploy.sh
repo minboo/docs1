bdea3ed3236381867b03aab5c03d107d824c2002
黄明步 <24766539@qq.com>
2022-09-11 16:21:57 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,31 @@
 set -e
 
 git add -A
-git commit -m 'update'
+git commit -m '1'
 git push -f git@github.com:minboo/docs.git
+
+git add -A
+git commit -m '2'
 git push -f git@gitee.com:minboo/docs.git
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy1'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:minboo/mingboo.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:minboo/docs.git master:pages
+cd -
 
 # 生成静态文件
 npm run docs:build
@@ -19,12 +41,11 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy2'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:minboo/mingboo.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:minboo/docs.git master:pages
 git push -f git@gitee.com:minboo/docs.git master:pages
 cd -
